a68dd003e97379a8b0f1d67f74e3ce698a50029f
djilou <ncdjilou@gmail.com>
2020-11-18 18:13:51 +0100

    play alaram only once

--- scraper_with_user_approval .py
+++ scraper_with_user_approval .py
@@ -90,6 +90,7 @@ ALLOWED_LICENSES = [
 ]
 products = {}
 product_num = 0
+alarm_counter = 0 
 for id in range(int(start_range), int(end_range)+1,-1):
     url = f'https://www.thingiverse.com/thing:{id}'
     if requests.get(url).status_code != 404:
@@ -114,7 +115,9 @@ for id in range(int(start_range), int(end_range)+1,-1):
 
         print(f"Product License :{License.get_attribute('text')}")
         if License.get_attribute('text') in ALLOWED_LICENSES:
-            os.system("alarm.mp3")
+            if alaram_counter != 0 :
+                os.system("alarm.mp3")
+                alaram_counter = alaram_counter + 1
             # remove user approval !
             continue_scrapping = input(f"Do you want to continue [y/n] : ")
             if continue_scrapping in ['y', 'Y']:
